74366428ebc950ff3bfd70fb5359cf7ad1ed0068
Harshil-Patel28 <76663814+Harshil-Patel28@users.noreply.github.com>
2022-01-24 16:15:12 -0500

    Added feature for mulitple cities in one run. Added inputs/break conditions.

--- GetWeather.py
+++ GetWeather.py
@@ -4,10 +4,19 @@ import requests
 API_KEY = "efb7ddca990388e37e96615e788890f2"
 URL = "http://api.openweathermap.org/data/2.5/weather"
 
-city = 'ottawa'
+run = True
+while run:
+    city = input("Please enter a city (Quit or Q to stop): ")
 
-res = requests.get(f"{URL}?appid={API_KEY}&q={city}")
-if res.status_code == 200:
-    print(res.json())
-else:
-    print("The city wasn't found please try again.")
+    if city.lower() == 'q' or city.lower() == 'quit':
+        run = False
+        break
+
+    res = requests.get(f"{URL}?appid={API_KEY}&q={city}")
+
+    if res.status_code == 200:
+        print(res.json())
+
+    else:
+
+        print("The city wasn't found please try again.")
