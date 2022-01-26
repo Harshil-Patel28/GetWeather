cc66ffb36d19f55260a65af2c5c1a125bb18fe85
Harshil-Patel28 <76663814+Harshil-Patel28@users.noreply.github.com>
2022-01-26 01:17:59 -0500

    Added feature that displays sunrise and sunset

--- GetWeather.py
+++ GetWeather.py
@@ -1,10 +1,8 @@
 from urllib import request
+import datetime
 import requests
 
 
-def kelvins_to_celcius(s):
-    num = int(s)
-    return num - 273.15
 
 API_KEY = "efb7ddca990388e37e96615e788890f2"
 URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -23,29 +21,28 @@ while run:
     if res.status_code == 200:
         city_dict = res.json()
         #print(city_dict)
-        print("The weather in " + city[0].upper() + city[1:] + ", " + city_dict['sys']['country'] +
+        print("\nThe weather in " + city[0].upper() + city[1:] + ", " + city_dict['sys']['country'] +
               " is " + str(round((city_dict['main']['temp'] - 273.15), 2))
               + "°C with " + city_dict['weather'][0]['description'])
-        #print(int((city_dict['timezone'] / 3600) + 5))
 
-        if '-' in str(int((city_dict['timezone'] / 3600) + 5)):
-            print(city[0].upper() + city[1:] + "'s time zone is " +
-                  str((int((city_dict['timezone'] / 3600) + 5)) * -1) +
+        print("Sunrise in " + city[0].upper() + city[1:] +": " +
+              str(datetime.datetime.fromtimestamp(city_dict['sys']['sunrise']).time()))
+
+        print("Sunset in " + city[0].upper() + city[1:] +": " +
+              str(datetime.datetime.fromtimestamp(city_dict['sys']['sunset']).time()))
+
+        if '-' in str(float((city_dict['timezone'] / 3600) + 5)):
+            print(city[0].upper() + city[1:] + "'s Timezone: " +
+                  str((float((city_dict['timezone'] / 3600) + 5)) * -1) +
                   " hours behind the Eastern Time Zone.")
 
-        elif str(int((city_dict['timezone'] / 3600) + 5)) == '0':
-            print(city[0].upper() + city[1:] + " is in the "
-                                               "Eastern Time Zone.")
-
+        elif str(float((city_dict['timezone'] / 3600) + 5)) == '0.0':
+            print(city[0].upper() + city[1:] + "'s Timezone: Eastern Time Zone.")
         else:
 
-            print(city[0].upper() + city[1:] + "'s time zone is " +
-                  str(int((city_dict['timezone'] / 3600) + 5)) +
+            print(city[0].upper() + city[1:] + "'s Timezone:" +
+                  str(float((city_dict['timezone'] / 3600) + 5)) +
                   " hours ahead of the Eastern Time Zone.")
-
-
-
-
 
         loop_guard = True
         while loop_guard:
